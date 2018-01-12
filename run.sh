439f326090fef7d47bb48e5493bda0949c6415fc
Paulo Silva <paulo308@users.noreply.github.com>
2018-01-12 17:27:48 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
 
 # Log the resulting configuration file
-cat /etc/nginx/conf.d/default.conf
+cat /tmp/default.conf
 
-# Start nginx
-service nginx start
+
+echo starting to built config
+
+
+echo replacing ___CERT_PATH___/$CERT_PATH
+echo replacing ___KEY_PATH___/$KEY_PATH
+echo replacing ___PROXY_IP___/$PROXY_IP
+echo replacing ___PROXY_PORT___/$PROXY_PORT
+
+sed -i "s/___CERT_PATH___/$CERT_PATH/g" /tmp/default.conf
+sed -i "s/___KEY_PATH___/$KEY_PATH/g" /tmp/default.conf
+sed -i "s/___PROXY_IP___/$PROXY_IP/g" /tmp/default.conf
+sed -i "s/___PROXY_PORT___/$PROXY_PORT/g" /tmp/default.conf
+
+cat /etc/nginx/default.conf
+
+
+
+# Log the resulting configuration file
+cat /tmp/default.conf
+
+mv /tmp/default.conf /etc/nginx/conf.d/default.conf
