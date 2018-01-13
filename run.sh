ff7594a83352beba2e71387f56a673076d223a2f
Paulo Silva <paulo308@users.noreply.github.com>
2018-01-13 14:04:34 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,11 +17,17 @@ sed -i "s@___KEY_PATH___@$KEY_PATH@g" /tmp/default.conf
 sed -i "s@___PROXY_IP___@$PROXY_IP@g" /tmp/default.conf
 sed -i "s@___PROXY_PORT___@$PROXY_PORT@g" /tmp/default.conf
 
-cat /etc/nginx/default.conf
 
 
 
+echo new conf
 # Log the resulting configuration file
 cat /tmp/default.conf
 
 mv /tmp/default.conf /etc/nginx/conf.d/default.conf
+
+
+echo new conf on nginx location
+cat /etc/nginx/default.conf
+
+nginx -g 'daemon off;'
